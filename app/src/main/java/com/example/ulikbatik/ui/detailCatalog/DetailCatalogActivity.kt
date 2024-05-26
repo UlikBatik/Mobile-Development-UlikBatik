e4dba7e2bfcb62ba2d0cc7c1c5c43f238dc56ccd
Salsabila Fauziah <89504889+salsabilafauzh@users.noreply.github.com>
2024-05-27 03:00:58 +0700

    slicing detail catalog batik, remove unused source in detail post

--- app/src/main/java/com/example/ulikbatik/ui/detailCatalog/DetailCatalogActivity.kt
+++ app/src/main/java/com/example/ulikbatik/ui/detailCatalog/DetailCatalogActivity.kt
@@ -1,4 +1,4 @@
-package com.example.ulikbatik.ui.detailPost
+package com.example.ulikbatik.ui.detailCatalog
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -7,11 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.ulikbatik.R
 
-class DetailPostActivity : AppCompatActivity() {
+class DetailCatalogActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_detail_post)
+        setContentView(R.layout.activity_detail_catalog)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
