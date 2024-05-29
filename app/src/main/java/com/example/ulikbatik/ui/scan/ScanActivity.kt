0caa0b063fdc36bbf3798c030256e1458bfcd257
Salsabila Fauziah <89504889+salsabilafauzh@users.noreply.github.com>
2024-05-29 15:30:28 +0700

    [feature] develop scan

--- app/src/main/java/com/example/ulikbatik/ui/scan/ScanActivity.kt
+++ app/src/main/java/com/example/ulikbatik/ui/scan/ScanActivity.kt
@@ -1,16 +1,34 @@
 package com.example.ulikbatik.ui.scan
 
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.ulikbatik.R
 import com.example.ulikbatik.databinding.ActivityScanBinding
+import java.io.File
 
 class ScanActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityScanBinding
+    private var file: File? = null
+    private var currentImageUri: Uri? = null
+
+    private val launcherGallery = registerForActivityResult(
+        ActivityResultContracts.PickVisualMedia()
+    ) { uri: Uri? ->
+        if (uri != null) {
+            currentImageUri = uri
+            showImage(uri)
+        } else {
+            showToast(getString(R.string.no_media_selected))
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,5 +41,26 @@ class ScanActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        setupAction()
+
     }
+
+    private fun setupAction() {
+        binding.apply {
+            galleryBtn.setOnClickListener {
+                launcherGallery.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+            }
+        }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun showImage(uri: Uri) {
+        binding.apply {
+            imageIv.setImageURI(uri)
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/ulikbatik/ui/scan/ScanViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.ulikbatik.ui.scan
+
+import androidx.lifecycle.ViewModel
+
+class ScanViewModel(
+
+) : ViewModel() {
+}
